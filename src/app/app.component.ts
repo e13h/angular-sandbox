fa26bf519d72b3c2511982adf5965984ed02e335
Evan Smith <evanphilipsmith@gmail.com>
2019-07-29 12:13:48 -0600

    added tabbed interface to switch between card and list view modes implements ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent {
     isFavorite: false
   };
   imageUrl = "http://placekitten.com/500/200";
+  viewMode = 'card';
 
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
